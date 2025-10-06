77f3e36784fa3372f4ff389283485b5895e050d9
Yashika Ahirwar <yashikaahirwar18@gmail.com>
2025-10-06 23:27:59 +0530

    Update DynamicNFTMarketplace.sol

--- contracts/DynamicNFTMarketplace.sol
+++ contracts/DynamicNFTMarketplace.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPD-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -234,3 +234,4 @@ contract DynamicNFTMarketplace is ERC721URIStorage, Ownable, ReentrancyGuard {
         return (ids, prices);
     }
 }
+
