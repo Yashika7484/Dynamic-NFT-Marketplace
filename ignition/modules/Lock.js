5c12826910e4799152e5de2a6b7753079f3d866b
Yashika Ahirwar <yashikaahirwar18@gmail.com>
2025-10-23 23:04:41 +0530

    Fix comment capitalization in Lock.js
    Corrected capitalization of 'Deployments' in comment.

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -1,4 +1,4 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
+// This setup uses Hardhat Ignition to manage smart contract Deployments .
 // Learn more about it at https://hardhat.org/ignition
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
