27ad845c26122d8aa807317eb79569c6942f7d91
Yashika Ahirwar <yashikaahirwar18@gmail.com>
2025-10-23 23:05:08 +0530

    Fix typo in comment about Hardhat Ignition

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -1,4 +1,4 @@
-// This setup uses Hardhat Ignition to manage smart contract Deployments .
+// This setup uses Hardhat Ignition to manage the smart contract Deployments .
 // Learn more about it at https://hardhat.org/ignition
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
